b77410f7f7130ba340a58d415d876cd99b789ed4
KevangPatel <kevangpatel@gmail.com>
2021-01-13 20:10:43 +0530

    initial commit

---
+++ src/app/model.ts
@@ -0,0 +1,15 @@
+export class practice{
+    id:number;
+    pract!:pract[]
+    constructor(){
+        this.id=0
+    }
+}
+
+export class pract{
+    sr!:number
+    name!:string
+    comment!:string
+    data!:string
+
+}
